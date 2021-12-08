671a9e018ad25d1de2413772da299316073a84e2
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-08 15:32:49 +1100

    Rm memory sanitizer test from `run_tests.sh`
    My crate doesn't use `MaybeUninit` and memory sanitizer reports
    uninitialized read from the std library.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,6 @@ cd $(dirname `realpath $0`)
 
 cargo test
 RUSTFLAGS='-Zsanitizer=address' cargo +nightly test
-RUSTFLAGS='-Zsanitizer=memory' cargo +nightly test
 
 export RUSTFLAGS='-Zsanitizer=thread'
 exec cargo +nightly test
