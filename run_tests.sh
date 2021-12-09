fe7b202f9e3b8fb67f41bc034fa06601c32c10bf
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-09 14:43:22 +1100

    Add `cargo +nightly miri test` to `run_tests.sh`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ cd $(dirname `realpath $0`)
 cargo test
 RUSTFLAGS='-Zsanitizer=address' cargo +nightly test
 
-export RUSTFLAGS='-Zsanitizer=thread'
-export RUST_TEST_THREADS=1
-exec cargo +nightly test
+RUSTFLAGS='-Zsanitizer=thread' RUST_TEST_THREADS=1 cargo +nightly test
+
+exec cargo +nightly miri test
