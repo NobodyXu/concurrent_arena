5bccf767a0ef9e290b54a374ba81a5a8678ae66c
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-09 16:30:38 +1100

    Disable miri test as it is waayyyy too slow
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -7,8 +7,9 @@ export RUST_TEST_THREADS=1
 cargo test -- --nocapture
 RUSTFLAGS='-Zsanitizer=address' cargo +nightly test -- --nocapture
 
-RUSTFLAGS='-Zsanitizer=thread' cargo +nightly test \
+export RUSTFLAGS='-Zsanitizer=thread'
+exec cargo +nightly test \
     -Z build-std --target --target x86_64-unknown-linux-gnu -- --nocapture
 
-export MIRIFLAGS="-Zmiri-disable-isolation"
-exec cargo +nightly miri test -- --nocapture
+#export MIRIFLAGS="-Zmiri-disable-isolation"
+#exec cargo +nightly miri test -- --nocapture
