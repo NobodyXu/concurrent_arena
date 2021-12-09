96e880ae96a08ae4196d6294bf934ccb89511a90
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-09 16:22:29 +1100

    Fix thread sanitizer: build-std to avoid false positive
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,7 @@ export RUST_TEST_THREADS=1
 cargo test -- --nocapture
 RUSTFLAGS='-Zsanitizer=address' cargo +nightly test -- --nocapture
 
-RUSTFLAGS='-Zsanitizer=thread' cargo +nightly test -- --nocapture
+RUSTFLAGS='-Zsanitizer=thread' cargo +nightly test \
+    -Z build-std --target --target x86_64-unknown-linux-gnu -- --nocapture
 
 exec cargo +nightly miri test -- --nocapture
