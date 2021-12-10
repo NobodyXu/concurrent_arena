971bfc13267d0e9ad538ecc08a963caa4409fb39
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-10 15:38:17 +1100

    Repeat tests in run_tests.sh to reliably reproduce error in CI
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -4,12 +4,20 @@ cd $(dirname `realpath $0`)
 
 export RUST_TEST_THREADS=1
 
-cargo test -- --nocapture
-RUSTFLAGS='-Zsanitizer=address' cargo +nightly test -- --nocapture
+rep=$(seq 1 10)
 
-export RUSTFLAGS='-Zsanitizer=thread'
-exec cargo +nightly test \
-    -Z build-std --target --target $(uname -m)-unknown-linux-gnu -- --nocapture
+for _ in $rep; do
+    cargo test -- --nocapture
+done
+
+for _ in $rep; do
+    RUSTFLAGS='-Zsanitizer=address' cargo +nightly test -- --nocapture
+done
+    
+for _ in $rep; do
+    RUSTFLAGS='-Zsanitizer=thread' cargo +nightly test \
+        -Z build-std --target --target $(uname -m)-unknown-linux-gnu -- --nocapture
+done
 
 #export MIRIFLAGS="-Zmiri-disable-isolation"
 #exec cargo +nightly miri test -- --nocapture
