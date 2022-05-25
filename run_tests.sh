78544d226390218e5484aead08a096d519b407c4
Jiahao XU <Jiahao_XU@outlook.com>
2022-05-25 11:31:48 +1000

    Add support for passing args to cargo in `./run_tests.sh`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -7,19 +7,19 @@ export RUST_TEST_THREADS=1
 rep=$(seq 1 10)
 
 for _ in $rep; do
-    cargo test -- --nocapture
+    cargo test $@ -- --nocapture
 done
 
 export RUSTFLAGS='-Zsanitizer=address'
 export RUSTDOCFLAGS="$RUSTFLAGS"
 for _ in $rep; do
-    cargo +nightly test -- --nocapture
+    cargo +nightly test $@ -- --nocapture
 done
 
 export RUSTFLAGS='-Zsanitizer=thread' 
 export RUSTDOCFLAGS="$RUSTFLAGS"
 for _ in $rep; do
-    cargo +nightly test \
+    cargo +nightly test $@ \
         -Z build-std \
         --target --target $(uname -m)-unknown-linux-gnu \
         --features thread-sanitizer \
