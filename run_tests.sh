22f6fe4df709e11e20ab205eb622332d1b740ba8
Jiahao XU <Jiahao_XU@outlook.com>
2023-05-15 20:36:56 +1000

    Improve CI
     - Enable rust backtrace for better err debugging experience
     - Turn warnings into errors
     - Disable incremental build since CI always builds from scratch
     - Use sparse crates.io registries
     - Rm use of unmaintained `actions-rs/toolchain`
     - Speedup `cargo-clippy` with `--no-deps`
     - Use `Swatinem/rust-cache@v2` for caching
     - Add workflow `msrv.yml` for checking msrv
     - Add workflow `minimal.yml` for checking with minimal version of deps
     - Fix shellcheck errors in `run_tests.sh`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -9,26 +9,33 @@ export RUST_TEST_THREADS=1
 rep=$(seq 1 10)
 
 for _ in $rep; do
+    # shellcheck disable=SC2068
     cargo test $@ -- --nocapture
 done
 
 export RUSTFLAGS='-Zsanitizer=address'
 export RUSTDOCFLAGS="$RUSTFLAGS"
 for _ in $rep; do
+    # shellcheck disable=SC2068
     cargo +nightly test $@ -- --nocapture
 done
 
 export RUSTFLAGS='-Zsanitizer=thread' 
 export RUSTDOCFLAGS="$RUSTFLAGS"
 
-target=$(rustc -vV | grep host | cut -d : -f 2)
+target="$(rustc -vV | grep host | cut -d : -f 2 | tr -d '[:space:]')"
 for _ in $rep; do
+    # shellcheck disable=SC2068
     cargo +nightly test $@ \
         -Z build-std \
-        --target $target \
+        --target "$target" \
         --features thread-sanitizer \
         -- --nocapture
 done
 
 #export MIRIFLAGS="-Zmiri-disable-isolation"
-#exec cargo +nightly miri test -- --nocapture
+#exec cargo +nightly miri \
+#    nextest run \
+#    -Z build-std \
+#    --target "$target" \
+#    --release
