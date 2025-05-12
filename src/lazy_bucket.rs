0ee930ad6e3ca584d01e74a80f656e00cb0a4cb1
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-05-13 01:54:06 +1000

    Create lazy_bucket.rs

---
+++ src/lazy_bucket.rs
@@ -0,0 +1,39 @@
+use crate::{Arc, bucket::Bucket};
+
+use std::{
+    num::NonZeroU32,
+    ptr::null_mut,
+    sync::{
+        Mutex,
+        atomic::{AomicPtr, Ordering},
+    },
+};
+
+#[derive(Debug)]
+struct LinkedBucket {
+    bucket: Bucket,
+    next: Arc<LinkedBucket>,
+}
+
+#[derive(Debug)]
+pub(crate) struct LazyBucket {
+    mutex: Mutex<()>,
+    /// Atomic Arc
+    head: AtomicPtr<LinkedBucket>,
+}
+
+impl LazyBucket {
+    pub const fn new() -> Self {
+        Self {
+            mutex: Mutex::new(()),
+            length: AtomicU32::new(0),
+            head: AtomicPtr::new(null_mut()),
+        }
+    }
+
+    pub fn allocate_buckets(&self, count: NonZeroU32) {}
+}
+
+impl Drop for LazyBucket {
+    fn drop(&mut self) {}
+}
